4f31d79910b8853f2e5c5f39eb328263c3caa998
christian-dale <jchaukvik@gmail.com>
2022-01-05 17:54:11 +0100

    Add menu element

---
+++ src/ui/menu.ts
@@ -0,0 +1,22 @@
+class Menu {
+    public menu: JQuery<HTMLElement>;
+
+    constructor(title: string = "") {
+        this.menu = Util.template("#template-menu", {}, "body");
+        this.createTitle(title);
+        this.menu.find(".menuClose").on("click", e => this.onClickClose(e));
+    }
+
+    private onClickClose(e: Event) {
+        this.menu.remove();
+    }
+
+    private createTitle(title: string) {
+        const elemTitle = $(`<h2 class="menuTitle">${title}</h2>`);
+        this.addElement(elemTitle);
+    }
+
+    public addElement(element: JQuery<HTMLElement> | HTMLElement | string) {
+        this.menu.append(element);
+    }
+}
