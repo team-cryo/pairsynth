5a21a3991fe460088acff4d6c7dd8b7237e3972e
christian-dale <jchaukvik@gmail.com>
2022-01-06 02:09:55 +0100

    Fix typescript type error in ui/menu

--- src/ui/menu.ts
+++ src/ui/menu.ts
@@ -4,7 +4,7 @@ class Menu {
     constructor(title: string = "") {
         this.menu = Util.template("#template-menu", {}, "body");
         this.createTitle(title);
-        this.menu.find(".menuClose").on("click", e => this.onClickClose(e));
+        this.menu.find(".menuClose").on("click", (e: Event) => this.onClickClose(e));
     }
 
     private onClickClose(e: Event) {
