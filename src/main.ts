49dcea0772c25f6f714c73fc1db4299dfbc4eddd
sigurd4 <sigurd-s@online.no>
2022-01-10 07:01:55 +0100

    Changed initial test conditions in main

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ const modman: ModuleManager = new ModuleManager();
 const midman: MIDIManager = new MIDIManager();
 
 const lfo1: ModuleOsc = modman.addModule(new ModuleOsc(Waveform.sine, ModuleOsc.rangeLFO, ModuleOsc.rangeLFO));
-//const lfo2: ModuleOsc = modman.addModule(new ModuleOsc(Waveform.square, ModuleOsc.rangeLFO));
+const lfo2: ModuleOsc = modman.addModule(new ModuleOsc(Waveform.sine, ModuleOsc.rangeLFO, ModuleOsc.rangeLFO));
+const lfo3: ModuleOsc = modman.addModule(new ModuleOsc(Waveform.sine, ModuleOsc.rangeLFO, ModuleOsc.rangeLFO));
 //const midi: ModuleMIDIKeyboard = modman.addModule(new ModuleMIDIKeyboard(midman));
 //const noise: ModuleNoise = modman.addModule(new ModuleNoise());
 const oscillator: ModuleOsc = modman.addModule(new ModuleOsc(Waveform.sine));
@@ -20,6 +21,8 @@ vca.getCV().setConnection(midi.getGate());
 vca.getInput().setConnection(oscillator.getOutput());*/
 //modman.getOutput(0).setConnection(vca.getOutput());
 
+lfo1.getCV().setConnection(lfo2.getOutput());
+lfo2.getCV().setConnection(lfo3.getOutput());
 oscillator.getCV().setConnection(lfo1.getOutput());
 modman.getOutput(0).setConnection(oscillator.getOutput());
 
